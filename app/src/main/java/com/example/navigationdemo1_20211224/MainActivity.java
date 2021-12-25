6f13270f869f8270620fc68d6d36097cc7ef029c
Sophie <wenxi1988@hotmail.com>
2021-12-25 01:13:21 -0500

    set and Activate <- back button at Nav bar

--- app/src/main/java/com/example/navigationdemo1_20211224/MainActivity.java
+++ app/src/main/java/com/example/navigationdemo1_20211224/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.navigationdemo1_20211224;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // set <- back button at Nav bar
+        NavController controller = Navigation.findNavController(this,R.id.fragmentContainerView3);
+        NavigationUI.setupActionBarWithNavController(this,controller);
+
+    }
+
+    // Activate <- back button at Nav bar
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController controller=Navigation.findNavController(this, R.id.fragmentContainerView3);
+        return controller.navigateUp();
+        //return super.onSupportNavigateUp();
     }
 }
