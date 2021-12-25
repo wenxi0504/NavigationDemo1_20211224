8a7ea315eb0203c60a1b7584cf2d8a74c2724170
Sophie <wenxi1988@hotmail.com>
2021-12-25 00:04:39 -0500

    create an app

---
+++ app/src/test/java/com/example/navigationdemo1_20211224/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.navigationdemo1_20211224;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
